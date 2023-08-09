b7c0fd9c35308afee44a121b13284867e7ef74c3
Zackon123-afk <arnaullaberia27@gmail.com>
2023-08-09 23:22:28 +0200

    Solution done with BFS Algorithm
    Solution in main.py using BFS Algorithm.
    Test 1 OK
    Test 2 OK
    Test 3 OK
    Test 4 No - finds the path, but not the optimal.
    
    Continue implementing with A*

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ WEST = 'W'
 SOUTH = 'S'
 MOVE_LIST = [EAST,NORTH,WEST,SOUTH]
 
-recently_rotated = False
-
 labyrinth = [[".",".",".",".",".",".",".",".","."],
             ["#",".",".",".","#",".",".",".","."],
             [".",".",".",".","#",".",".",".","."],
@@ -38,29 +36,31 @@ labyrinth4 = [[".",".",".",".",".",".",".",".",".","."],
 def solve_maze(maze):
     
     def is_valid(x, y, direction):
-        global recently_rotated
-        if 0 <= x < rows and 0 <= y < cols and (maze[x][y] == "." or recently_rotated == True):
-            recently_rotated = False
+        if 0 <= x < rows and 0 <= y < cols and maze[x][y] == ".":
             if direction == HORIZONTAL:
                 return (0 <= y+1 < cols and 0 <= y-1 < cols and maze[x][y+1] != "#"
                 and maze[x][y-1] != "#")
             else:
                 return (0 <= x+1 < rows and 0 <= x-1 < rows and maze[x+1][y] != "#"
                 and maze[x-1][y] != "#")
-            
+    
+    def is_goal(x, y, direction):
+        return ((x == rows - 2 and y == cols - 1 and direction == VERTICAL) or
+                (x == rows - 1 and y == cols - 2 and direction == HORIZONTAL))     
+
     def rotate(direction):
         return HORIZONTAL if direction == VERTICAL else VERTICAL
 
+    def can_rotate(x,y):
+        return (1 <= x < rows-1 and 1 <= y < cols-1 and maze[x][y] != "#" and maze[x+1][y] != "#" and maze[x-1][y] != "#" and maze[x][y+1] != "#" 
+                and maze[x][y-1] != "#" and maze[x+1][y+1] != "#" and maze[x-1][y-1] != "#" and maze[x+1][y-1] != "#" and maze[x-1][y+1] != "#")
     
     def dfs(x, y, direction, steps):
 
-        global recently_rotated
-
         if not is_valid(x, y, direction):
-            recently_rotated = False
             return False, steps
         
-        if x == rows - 2 and y == cols - 1 and direction == VERTICAL:
+        if is_goal(x,y,direction):
             return True, steps
 
         maze[x][y] = 'X'  # Mark the cell as visited
@@ -75,20 +75,28 @@ def solve_maze(maze):
                 new_x, new_y = x - 1, y
             elif move == WEST:
                 new_x, new_y = x, y - 1
-            
-            recently_rotated = False
+
             result, new_steps = dfs(new_x, new_y, direction, steps + 1)
             if result:
                 return result, new_steps
         
-        # Rotate
-        new_direction = rotate(direction)  # Rotate 90 degrees counterclockwise
-        recently_rotated = True
-        result, new_steps = dfs(x, y, new_direction, steps + 1)
-        if result:
-            return result, new_steps
+        # Now moving with the rod rotated
+        for move in MOVE_LIST:
+            if move == EAST:
+                new_x, new_y = x, y + 1
+            elif move == SOUTH:
+                new_x, new_y = x + 1, y
+            elif move == NORTH:
+                new_x, new_y = x - 1, y
+            elif move == WEST:
+                new_x, new_y = x, y - 1
+            
+            if can_rotate(new_x,new_y):
+                new_direction = rotate(direction)
+                result, new_steps = dfs(new_x, new_y, new_direction, steps + 2)
+                if result:
+                    return result, new_steps
         
-        recently_rotated = False
         maze[x][y] = '.'  # Backtrack if the path doesn't lead to the destination
         return False, steps
     
@@ -98,14 +106,14 @@ def solve_maze(maze):
     start_direction = HORIZONTAL
 
     _, steps = dfs(0, 1, start_direction, 0)
-    
-    return steps
+
+    return steps if steps != 0 else -1
 
 def main():
     maze=labyrinth
 
     num_pasos = solve_maze(maze)
-
+    
     print(num_pasos)
 
 if __name__ == "__main__":
